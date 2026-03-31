fff8efb4c47b5e79714509091507e22693ae9475
Вероника Мазуренко <veronikavv1732@gmail.com>
2026-03-31 21:41:52 +0300

    add test airplane

--- tests/test_aeraplane.py
+++ tests/test_aeraplane.py
@@ -2,47 +2,47 @@ import unittest
 
 import pytest
 
-from src.aeraplane import Aeroplane
+from src.airplane import Airplane
 
 
-class TestAeroplane(unittest.TestCase):
-    """ класс тестирует Aeroplane """
+class TestAirplane(unittest.TestCase):
+    """ класс тестирует Airplane """
 
-    def test_aeroplane_init(self):
-        aeroplane = Aeroplane(callsign="N5641X", country="United States", velocity=341.57,
+    def test_airplane_init(self):
+        airplane = Airplane(callsign="N5641X", country="United States", velocity=341.57,
                           geo_altitude=10203.18)
 
-        self.assertEqual(aeroplane.callsign, "N5641X")
-        self.assertEqual(aeroplane.country, "United States")
-        self.assertEqual(aeroplane.velocity, 341.57)
-        self.assertEqual(aeroplane.geo_altitude, 10203.18)
+        assert airplane.callsign == "N5641X"
+        assert airplane.country == "United States"
+        assert airplane.velocity == 341.57
+        assert airplane.geo_altitude == 10203.18
 
 
     def test_comparison_velocity_geo_altitude_1(self):
         """Тестирование магического метода __ge__, __eq__ (сравнение объектов)."""
-        aeroplane1 = Aeroplane(callsign="N5641X", country="United States", velocity=341.57,
+        airplane1 = Airplane(callsign="N5641X", country="United States", velocity=341.57,
                            geo_altitude=10203.18)
-        aeroplane2 = Aeroplane(callsign="PVL832", country="Canada", velocity=341.58,
+        airplane2 = Airplane(callsign="PVL832", country="Canada", velocity=341.58,
                            geo_altitude=10203.19)
 
-        self.assertFalse(aeroplane1 == aeroplane2)
-        self.assertTrue(aeroplane2 >= aeroplane1)
+        assert airplane1 != airplane2
+        assert airplane2 >= airplane1
 
 
     def test_comparison_velocity_geo_altitude_2(self):
         """Тестирование сравнения объектов с одинаковыми значениями"""
-        aeroplane1 = Aeroplane(callsign="N5641X", country="United States", velocity=341.57,
+        airplane1 = Airplane(callsign="N5641X", country="United States", velocity=341.57,
                            geo_altitude=10203.19)
-        aeroplane2 = Aeroplane(callsign="PVL832", country="Canada", velocity=341.57,
+        airplane2 = Airplane(callsign="PVL832", country="Canada", velocity=341.57,
                            geo_altitude=10203.19)
 
-        self.assertTrue(aeroplane1 == aeroplane2)
-        self.assertTrue(aeroplane2 >= aeroplane1)
+        assert airplane1 == airplane2
+        assert airplane2 >= airplane1
 
 
-    def test_aeroplane_str_method(self):
+    def test_airplane_str_method(self):
         """Тестирование метода __str__ для корректного вывода строки."""
-        aeroplane = Aeroplane("UAL1621", "United States", 268.79, 10203.18)
+        airplane = Airplane("UAL1621", "United States", 268.79, 10203.18)
 
         expected_str = "Позывной рейса: UAL1621, страна регистрации ВС: United States, горизонтальная скорость: 268.79 м/с, геометрическая высота: 10203.18 м."
-        self.assertEqual(str(aeroplane), expected_str)
+        assert str(airplane) == expected_str
